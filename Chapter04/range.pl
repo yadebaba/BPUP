e00330273a7b1a130e1fc07322e12c1f69eaa34f
yadebaba <hj@uestc.edu.cn>
2020-11-14 01:27:32 +0800

    BPUP

--- Chapter04/range.pl
+++ Chapter04/range.pl
@@ -3,7 +3,7 @@
 use v5.10;
 
 #range operator in scalar context
-@base = qw (ade0i0e cytosine guanine thymine uracil any);
+@base = qw (adenine cytosine guanine thymine uracil any);
 foreach (@base) {  
 	say "$r\t$_" if ( $r = /a/ .. /n/); 
 }
