fc53674500d61a3a9a2426657145ed33fccb9895
yadebaba <hj@uestc.edu.cn>
2022-01-08 22:15:05 +0800

    BPUP

--- Chapter04/x.pl
+++ Chapter04/x.pl
@@ -12,7 +12,7 @@ say '-' x 14;
 
 @residue = qw(A C D E F G H I K L M N P Q R S T V W Y);
 
-# amino acids property scale taken from the AAIndex database
+# property scale taken from the AAIndex database
 say "residue volume (Bigelow, 1967)";
 @volume=(52.6,68.3,68.4,84.7,113.9,36.3,91.9,102.0,105.1,
 102.0,97.7,75.7,73.6,89.7,109.1,54.9,71.2,85.1,135.4,116.2);
