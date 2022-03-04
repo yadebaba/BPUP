6ad1a24268be2d6dc3e28c1cce80d2928f352697
yadebaba <hj@uestc.edu.cn>
2022-03-05 01:09:35 +0800

    BPUP

---
+++ Chapter05/glob.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use v5.10;
+
+$g = "MH370";
+@g = qw(tom jerry);
+%g = (H=>1, O=>18);
+sub g {say "hello GLOB!";}
+open ("g","poem.txt");
+
+# typeglob is a special hash
+say *g{SCALAR},"=>${*g{SCALAR}}";
+say *g{ARRAY},"=>@{*g{ARRAY}}";
+say *g{HASH},"=>",%{*g{HASH}};
+say *g{CODE},"=>", &{*g{CODE}};
+say *g{IO};
+say *g{GLOB};
+say *g{PACKAGE};
+say *g{NAME};
+
+# create an alias for all data types
+*a = *g;
+print while <a>;
