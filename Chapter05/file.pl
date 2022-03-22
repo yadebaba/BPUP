2773bd229f62e4a3762d638e4a0f1dbc0be1d6ee
yadebaba <hj@uestc.edu.cn>
2022-03-22 18:53:44 +0800

    play with files

--- Chapter05/file.pl
+++ Chapter05/file.pl
@@ -2,8 +2,9 @@
 use v5.10;
 use File::Copy;
 
-rename "mimotope.txt", "mimotope.fa" or die $!;
-move "mimotope.fa", "mimotope.seq" or die $!;
-copy "mimotope.seq", "mimotope0.seq" or die $!;
+copy "mimotope1.fa", "mimotope1.seq" or die $!;
+rename "mimotope1.seq", "mimotope.seq" or die $!;
+move "mimotope.seq", "mimotope0.seq" or die $!;
+copy "mimotope0.seq", "mimotope2.seq" or die $!;
 say foreach <mimotope*>;
 say unlink glob "*.seq";
