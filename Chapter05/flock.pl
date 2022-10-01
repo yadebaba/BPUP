204084c333ef6374b581306f93a1bad1a656c282
yadebaba <hj@uestc.edu.cn>
2022-10-01 23:09:23 +0800

    Demo of the flock function

--- Chapter05/flock.pl
+++ Chapter05/flock.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl
 use v5.10;
 
-open POEM, "< poem.txt" || die "$!";
-flock(POEM, 2) || die "$!";
+open POEM, "< poem.txt" || die "$!\n";
+flock(POEM, 2) || die "$!\n";
 say scalar localtime;
 say "The file poem.txt is locked for reading by pid=$$.";
 sleep 20;
-flock(POEM, 8) || die "$!";
+flock(POEM, 8) || die "$!\n";
 say scalar localtime;
 say "Now poem.txt is unlocked for reading by pid=$$.";
 close POEM;
