98764aa1b912d741f4ba6bba36a9f5e2adce62b5
yadebaba <hj@uestc.edu.cn>
2022-10-01 21:57:05 +0800

    Play with images

--- Chapter05/binmode.pl
+++ Chapter05/binmode.pl
@@ -3,15 +3,15 @@
 # Working with a binary file on Windows system
 # the binmode call or :raw layer is necessary  
 
-open IN, "brain.jpg" || die $!;
-open OUT, ">:raw", "brain1.jpg" || die $!;
+open IN, "brain.jpg" || die "$!\n";
+open OUT, ">:raw", "brain1.jpg" || die "$!/n";
 print OUT while <IN>;
 
-open IN, "brain.jpg" || die $!;
-open OUT, ">", "brain2.jpg" || die $!;
+open IN, "brain.jpg" || die "$!\n";
+open OUT, ">", "brain2.jpg" || die "$!\n";
 print OUT while <IN>;
 
-open IN, "brain.jpg" || die $";
-open OUT, ">", "brain3.jpg" || die $!;
+open IN, "brain.jpg" || die "$!\n";
+open OUT, ">", "brain3.jpg" || die "$!\n";
 binmode OUT; select OUT;
 print while <IN>;
