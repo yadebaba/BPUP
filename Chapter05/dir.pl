ba1999fa917a4caa6947e341b271adae0e4aebbc
yadebaba <hj@uestc.edu.cn>
2022-10-01 22:58:12 +0800

    Play with directories

--- Chapter05/dir.pl
+++ Chapter05/dir.pl
@@ -6,15 +6,15 @@ use File::Path;
 use Cwd;
 
 say cwd();
-mkdir "toy" or die $!;
-chdir "toy" or die $!;
+mkdir "toy" or die "$!\n";
+chdir "toy" or die "$!\n";
 say cwd();
-mkdir "decoy" or die $!;
+mkdir "decoy" or die "$!\n";
 `ls -al >dir.txt`; system ("tree");
 
-open (STDOUT, "| tee -ai log.txt" ) or die $!;
+open (STDOUT, "| tee -ai log.txt" ) or die "$!\n";
 my $dir = getcwd;
-opendir(my $dh, $dir) or die $!;
+opendir(my $dh, $dir) or die "$!\n";
 say ("\nPosition before read: ", telldir($dh) );
 my $counter =1;
 while(readdir $dh) {  
